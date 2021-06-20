7fb25beaae5a29862e8a62da2797695bafeb5648
Anup Singh <anups5246@gmail.com>
2021-06-20 23:01:29 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// let burgerMenuBtn = document.querySelector('#burger');
+// let closeBtn = document.querySelector('#close');
+// let menu = document.querySelector('.menu');
+
+// //to open the menu
+// burgerMenuBtn.addEventListener('click', () => {
+
+//     if (menu.classList.contains('close')) {
+//         menu.classList.remove('close');
+//     }
+//     if (!burgerMenuBtn.classList.contains('hidden')) {
+//         burgerMenuBtn.classList.add('hidden');
+//     }
+//     if (closeBtn.classList.contains('hidden')) {
+//         closeBtn.classList.remove('hidden');
+//     }
+// })
+// closeBtn.addEventListener('click', () => {
+//     if (burgerMenuBtn.classList.contains('hidden')) {
+//         burgerMenuBtn.classList.remove('hidden');
+//     }
+//     if (!closeBtn.classList.contains('hidden')) {
+//         closeBtn.classList.add('hidden');
+//         menu.classList.add('close');
+//     }
+// })
