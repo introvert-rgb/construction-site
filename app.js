98a25ed541124ecd141e4e519ab558bf475b1cee
Anup Singh <anups5246@gmail.com>
2021-06-23 13:48:08 +0530

    hero done

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 let menu = document.querySelector('.menu');
+let nav = document.querySelector('nav');
+window.onscroll = position;
+
+function position() {
+    console.log(pageYOffset);
+
+    if (window.pageYOffset > 40) {
+        console.log(pageYOffset);
+        nav.classList.add('bg-white');
+    } else {
+        nav.classList.remove('bg-white')
+    }
+
+}
+// console.log(offset);
 
 let header = document.querySelector('.header');
 let menuBtn = document.querySelector('.menu-hamburger');
