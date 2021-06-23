d9e4f267547867d78153101cf57d6bde548bd12e
Anup Singh <anups5246@gmail.com>
2021-06-23 15:49:44 +0530

    changes

--- app.js
+++ app.js
@@ -21,11 +21,11 @@ menuBtn.addEventListener('click', () => {
     if (menu.classList.contains('open')) {
         menu.classList.remove('open');
         menuBtn.classList.add('close');
-        header.classList.add('fixed');
+
     } else {
         menu.classList.add('open');
         menuBtn.classList.remove('close');
-        header.classList.remove('fixed');
+
 
     }
 })
