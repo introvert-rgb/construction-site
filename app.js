e43dd20b49f740d81c8bdba12ad9117c81eb306a
Anup Singh <anups5246@gmail.com>
2021-06-24 15:41:24 +0530

    reviews

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 let menu = document.querySelector('.menu');
 let nav = document.querySelector('nav');
+
 window.onscroll = position;
 
 function position() {
-    console.log(pageYOffset);
+
 
     if (window.pageYOffset > 40) {
-        console.log(pageYOffset);
+
         nav.classList.add('bg-white');
     } else {
         nav.classList.remove('bg-white')
@@ -17,15 +18,18 @@ function position() {
 
 let header = document.querySelector('.header');
 let menuBtn = document.querySelector('.menu-hamburger');
+let scrollLock = document.querySelector('.header');
 menuBtn.addEventListener('click', () => {
     if (menu.classList.contains('open')) {
         menu.classList.remove('open');
         menuBtn.classList.add('close');
+        scrollLock.classList.add('fixed');
+
 
     } else {
         menu.classList.add('open');
         menuBtn.classList.remove('close');
-
+        scrollLock.classList.remove('fixed');
 
     }
 })
